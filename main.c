04c87cccf529d26d11cc0dfe54fa89a852f1820e
David Sawatzke <d-git@sawatzke.dev>
2023-12-02 00:19:22 +0100

    Add WS2812 output

--- main.c
+++ main.c
@@ -3,9 +3,35 @@
 #include <stdio.h>
 #include <string.h>
 
+#define WS2812DMA_IMPLEMENTATION
+// #define WSRBG //For WS2816C's.
+#define WSGRB // For SK6805-EC15
+#define NR_LEDS 6
+
+#include "ws2812b_dma_spi_led_driver.h"
+
+#include "color_utilities.h"
+
+// Callbacks that you must implement.
+uint32_t WS2812BLEDCallback(int ledno) {
+  uint8_t index = (phases[ledno]) >> 8;
+  uint8_t rsbase = sintable[index];
+  uint8_t rs = rsbase >> 3;
+  uint32_t fire = ((huetable[(rs + 190) & 0xff] >> 1) << 16) |
+                  (huetable[(rs + 30) & 0xff]) |
+                  ((huetable[(rs + 0)] >> 1) << 8);
+
+  return fire
+}
+
 int main() {
   SystemInit();
   usb_setup();
+  WS2812DMAInit();
+  frameno = 0;
+  for (k = 0; k < NR_LEDS; k++)
+    phases[k] = k << 8;
+  int tweendir = 0;
   while (1) {
 #if RV003USB_EVENT_DEBUGGING
     uint32_t *ue = GetUEvent();
@@ -13,6 +39,35 @@ int main() {
       printf("%lu %lx %lx %lx\n", ue[0], ue[1], ue[2], ue[3]);
     }
 #endif
+    while (WS2812BLEDInUse)
+      ;
+
+    frameno++;
+
+    if (frameno == 1024) {
+      tweendir = 1;
+    }
+    if (frameno == 2048) {
+      tweendir = -1;
+      frameno = 0;
+    }
+
+    if (tweendir) {
+      int t = tween + tweendir;
+      if (t > 255)
+        t = 255;
+      if (t < -NR_LEDS)
+        t = -NR_LEDS;
+      tween = t;
+    }
+
+    for (k = 0; k < NR_LEDS; k++) {
+      phases[k] += ((((rands[k & 0xff]) + 0xf) << 2) +
+                    (((rands[k & 0xff]) + 0xf) << 1)) >>
+                   1;
+    }
+
+    WS2812BDMAStart(NR_LEDS);
   }
 }
 
