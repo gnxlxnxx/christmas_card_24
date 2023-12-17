1b27bc845b55c8ff8af245a844ffb8d1ce5709d3
David Sawatzke <d-git@sawatzke.dev>
2023-12-17 18:02:10 +0100

    small cleanup

--- main.c
+++ main.c
@@ -170,14 +170,11 @@ int main(void) {
   GPIOC->CFGLR |= GPIO_CNF_OUT_OD_AF << (4 * 6); // Fix WS2812 levels, needs 10K/1K pullup across Din and Vdd
   usb_setup();
   timer_matrix_init();
-  // Each collumn is hooked up to a timer PWM output, use a separate timer to
-  // schedule changing the row
   frameno = 0;
   int k;
   for (k = 0; k < NR_LEDS; k++)
     phases[k] = k << 8;
   int tweendir = 0;
-  /* matrix_data[1][1] = 128; */
   int ws2812counter = 0;
 
   read_touches();
